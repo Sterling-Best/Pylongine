d8af4030b0c126dc0894a0b9706ae4b5c95a5c16
Sterling Best <sterling@mysix.com>
2023-11-07 12:57:25 -0500

    Scene Manager added changes for the current scene and get_current_scene()
    Scene Controller, starting initiation of varios scenes

--- scene_systems/scenecontroller.py
+++ scene_systems/scenecontroller.py
@@ -1,4 +1,5 @@
 from scene_systems.scenemanager import SceneManager
+from scene_systems.scene_object_pool_manager import SceneObjectPoolManager
 from typing import Callable
 
 
@@ -6,9 +7,11 @@ class SceneController:
 
     _instance = None
 
+    scene_manager: SceneManager
+    scene_object_pool_manger: SceneObjectPoolManager
+
     def __new__(cls):
         # Setup for Singleton
-        # PhysicsEngine should be singleton
         if cls._instance is None:
             cls._instance = super(SceneController, cls).__new__(cls)
             cls._instance._initialized = False
@@ -21,6 +24,7 @@ class SceneController:
 
     def initiate_scene(self, arg_scene_name: str):
         pass
+
 
 
 
--- scene_systems/scenemanager.py
+++ scene_systems/scenemanager.py
@@ -32,10 +32,13 @@ class SceneManager:
     def get_scene(self, arg_scene_name: str) -> Scene:
         return self.scene_pool[arg_scene_name]
 
+    def get_current_scene(self) -> Scene:
+        return self.current_scene
+
     def remove_scene(self, arg_scene_name: str) -> None:
         self.scene_pool.pop(arg_scene_name)
 
-    def load_scene(self, arg_scene_name: str):
+    def load_scene(self, arg_scene_name: str) -> None:
         self.current_scene = self.scene_pool[arg_scene_name]
         self.current_scene.load_scene()
 
