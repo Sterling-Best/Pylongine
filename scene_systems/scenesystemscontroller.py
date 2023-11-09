2ba1e9e622602ff1097082aca7beb0ce35bb5737
Sterling Best <sterling@mysix.com>
2023-11-09 17:40:20 -0500

    Added the various controllers to the master SceneSystemsController

--- scene_systems/scenesystemscontroller.py
+++ scene_systems/scenesystemscontroller.py
@@ -1,5 +1,15 @@
+from scene_systems.scenecontroller import SceneController
+from scene_systems.sceneobjectpoolcontroller import SceneObjectPoolController
+from scene_systems.sceneobjectcontroller import SceneObjectController
+
 
 class SceneSystemsController:
 
+    scene_controller: SceneController
+    scene_object_pool_controller: SceneObjectPoolController
+    scene_object_controller: SceneObjectController
+
     def __init__(self):
-        pass
+        scene_controller = SceneController()
+        scene_object_pool_controller = SceneObjectPoolController()
+        scene_object_controller = SceneObjectController()
